0266e563565dd9107ef11f47980c96e09a086efb
JefAlive <jeferson.kersten@gmail.com>
2023-08-10 17:36:58 -0300

    using zod for validating todos input

--- src/entities/Todo.ts
+++ src/entities/Todo.ts
@@ -2,4 +2,8 @@ export class Todo {
   constructor(props: Todo) {
     Object.assign(this, props);
   }
+
+  public readonly id?: string;
+  public status?: string;
+  public description?: string;
 }
--- src/useCases/Todos/CreateTodo/CreateTodoController.ts
+++ src/useCases/Todos/CreateTodo/CreateTodoController.ts
@@ -1,12 +1,20 @@
 import { CreateTodoUseCase } from "./CreateTodoUseCase"
+import { z } from "zod"
 
 export class CreateTodoController {
   constructor(
     private createTodoUseCase: CreateTodoUseCase
   ) {}
 
-  handler = async () => {
-    await this.createTodoUseCase.execute({});
+  handler = async (event) => {
+    const todoSchema = z.object({
+      description: z.string()
+        .min(1, { message: "Must be 1 or more characters long" })
+        .max(255, { message: "Must be 255 or fewer characters long" })
+    })
+    const todo = todoSchema.parse(JSON.parse(event.body))
+
+    await this.createTodoUseCase.execute(todo)
 
     return {
       statusCode: 200,
