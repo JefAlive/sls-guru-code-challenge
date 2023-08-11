0b28d27abd72a604740f5babce944cb6804e9a8f
JefAlive <jeferson.kersten@gmail.com>
2023-08-11 17:38:58 -0300

    changed to deploy dynamodb local from docker instead of java version

--- src/useCases/Todos/e2e.spec.ts
+++ src/useCases/Todos/e2e.spec.ts
@@ -2,10 +2,6 @@ import { vi, expect, test, beforeEach, describe } from 'vitest'
 import createHandler from './CreateTodo/handler'
  
 describe('integration tests', () => {
-  beforeEach(() => {
-    // do something to truncate db
-  })
-
   test('create todo', async () => {
     const result = await createHandler({
       body: JSON.stringify({
